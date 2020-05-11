c856123a67a5069965191fdc0d5b4367a1af5af5
oriolmainouicunillera <omainou19@jaumebalmes.net>
2020-05-11 17:08:04 +0200

    Controlar els errors.

--- app/Http/Controllers/ProducteController.php
+++ app/Http/Controllers/ProducteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Producte;
+use App\Categoria;
 use Illuminate\Http\Request;
 
 class ProducteController extends Controller
@@ -19,9 +20,15 @@ class ProducteController extends Controller
 
     public function getAllCategoria($nom)
     {
+        // No funciona
+        $idCategoria = Categoria::query()
+            ->where('nom', $nom)
+            ->get('id');
+
         $productes = Producte::query()
-            ->where('categoria','=', $nom)
+            ->where('categoria_id', $idCategoria)
             ->get();
+
         return $productes;
     }
 
