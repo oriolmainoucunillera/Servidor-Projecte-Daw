38ed579dd1e4181702e6ab207ccfb232ac98936b
oriolmainouicunillera <omainou19@jaumebalmes.net>
2020-05-12 19:31:22 +0200

    Marca controller (no pujat en el anterior commit)

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Marca;
+
+class MarcaController extends Controller
+{
+    public function getAllMarques()
+    {
+        return Marca::all();
+    }
+
+    public function getMarca($id)
+    {
+        return Marca::find($id);
+    }
+
+    public function addMarca(Request $request)
+    {
+        $marques = Marca::create($request->all());
+        return $marques;
+    }
+
+    public function editMarca($id, Request $request)
+    {
+        $marques = Marca::findOrFail($id);
+        $marques->update($request->all());
+        return $marques;
+    }
+}
